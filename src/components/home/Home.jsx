fa47fa7485b044d08301def1f408ec3f3da70499
Jishnuck2000 <jeochirrakkal26@gmail.com>
2024-01-08 14:31:37 +0530

    Initial commit

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,271 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div>
+      <nav className="container">
+        <div className="container2"><img src='letter-p.png' className="logo"></img></div>
+        <div className="container1"></div>
+
+        <div className="container3">
+          {" "}
+          <img src="round-carpet.png" className="img1"></img>
+          <p className="p1">XYZ Enterprises Pvt.Ltd</p>
+        </div>
+        <div className="container4">
+          <img src="arrow.png" className="img2"></img>{" "}
+        </div>
+      </nav>
+      <div className="container5">
+        <div className="container6">
+          <div className="container7">
+            <div className="container8">
+              <img src="elon musk.jpg" className="img3"></img>
+
+              <div className="p-name">
+                Elon Musk <img src="bluearrow.png" className="img4"></img>
+              </div>
+                <p className="p-mail">elonmuskspaceX@gmail.com</p>
+            </div>
+
+            <div className="div-1">
+            <div ><img src="dashboard.png" className="dashboard"></img> 
+            <p className="div-2">Dashboard</p>
+            </div>
+            </div>
+
+            <div className="div-3">
+            <img src="flower-pot.png" className="flowerpot"></img>
+            <p className="div-4">Perks</p>
+            </div>
+
+            <div className="div-5">
+            <img src="puzzle.png" className="puzzle"></img>
+            <p className="div-6">Addons</p>
+            </div>
+
+            <div className="div-7">
+            <img src="faq.png" className="faq"></img>
+            <p className="div-8">FAQ</p>
+            </div>
+
+            <div className="div-9">
+            <img src="feedback.png" className="support"></img>
+            <p className="div-10">Support</p>
+            </div>
+
+            <div className="div-11">
+              <p className="logout">Logout<img src='logout.png' className="logoutimg"></img></p>
+              
+            </div>
+        
+
+          </div>
+
+<p className="heading">Choose a plan that's just right for you!</p>
+
+<div className="toggle-btn">
+  <div className="toggle2"><p className="monthly">Monthly</p></div>
+<p className="anually">Annually<sup className="sup">-10%</sup></p>
+</div>
+
+
+<div className="card1">
+  <p className="basic">Basic</p>
+  <p className="price-1">$ 899.99/mo</p>
+    <p className="price-2">$ 9.99/mo</p>
+    <div className="yellow-bg">
+      <p className="yellow-p">Get Started</p><img src="rightarrow.png" className="y-a"></img>
+      
+    </div>
+   <div className="hr-1">
+   <p className="cardcontent-1">What you'll get:</p>
+
+   <img src="userhead.png" className="userhead"></img>  
+    <p className="cardcontent-2">Upto 25 Users</p>
+    <img src="cloud-upload.png" className="userhead"></img>  
+    <p className="cardcontent-2">Upto 25gb Storage</p>
+    <img src="email.png" className="userhead"></img>  
+    <p className="cardcontent-2">Email Support</p>
+    <u><p className="cardcontent-3">EXPLORE FEATURES     <img src="yellowarrow.png" className="yellowarrow"></img>  
+</p></u>
+
+
+
+
+
+
+   </div>
+
+
+
+
+</div>    
+
+
+<div className="card2">
+  
+<p className="basic-1">Standard</p>
+<p className="price-3">$ 899.99/mo</p>
+<p className="price-4">$ 9.99/mo</p>
+<div className="red-bg">
+      <p className="yellow-p">Get Started</p><img src="rightarrow.png" className="y-a"></img>
+      
+    </div>
+
+
+    <div className="hr-1">
+   <p className="cardcontent-1">What you'll get:</p>
+
+   <img src="userhead.png" className="userhead"></img>  
+    <p className="cardcontent-2">Upto 50 Users</p>
+    <img src="cloud-upload.png" className="userhead"></img>  
+    <p className="cardcontent-2">Upto 60gb Storage</p>
+    <img src="email.png" className="userhead"></img>  
+    <p className="cardcontent-2">Email+Chat Support</p>
+    <u><p className="cardcontent-3">EXPLORE FEATURES     <img src="redarrow.png" className="yellowarrow"></img>  
+</p></u>
+
+
+
+
+
+
+   </div>
+
+
+
+  
+</div>
+
+<div className="card3">
+<p className="basic">Primium</p>
+<p className="price-5">$ 899.99/mo</p>
+<p className="price-6">$ 9.99/mo</p>
+<div className="violet-bg">
+      <p className="yellow-p">Get Started</p><img src="rightarrow.png" className="y-a"></img>
+      
+    </div>
+
+    <div className="hr-1">
+   <p className="cardcontent-1">What you'll get:</p>
+
+   <img src="userhead.png" className="userhead"></img>  
+    <p className="cardcontent-2">Upto 75 Users</p>
+    <img src="cloud-upload.png" className="userhead"></img>  
+    <p className="cardcontent-2">Upto 100gb Storage</p>
+    <img src="email.png" className="userhead"></img>  
+    <p className="cardcontent-2">Email+Chat+Whatsapp Support</p>
+    <u><p className="cardcontent-3">EXPLORE FEATURES     <img src="violetarrow.png" className="yellowarrow"></img>  
+</p></u>
+
+
+
+
+
+
+   </div>
+</div>
+
+
+
+<div className="card4">
+<div className="forever">
+  Free Forever
+</div>
+<p className="starter">Free Starter</p>
+<p className="para-1">The quickest and easiest way to try Protocols with basic functionalities</p>
+
+<div className="green-bg">
+<p className="yellow-p">Get Started</p><img src="rightarrow.png" className="y-a"></img>
+
+</div>
+
+<p className="get">What you'll get:</p>
+<img src="userhead.png" className="userhead1"></img>  
+    <p className="cardcontent-21">Upto 8 Users</p>
+
+    <img src="cloud-upload.png" className="userhead3"></img>  
+    <p className="cardcontent-211">Upto 3gb Storage</p>
+
+    <img src="email.png" className="userhead4"></img>  
+    <p className="cardcontent-2111">Email Support</p>
+
+    <img src="doubletick.png" className="userhead5"></img> 
+    <p className="cardcontent-21111">Basics of Documents,Task Flow,<br></br>Voting,Accounting,Banking,Notes,<br></br>Investor,Director and Team<br></br>Management included</p>
+ 
+
+
+</div>
+
+
+
+
+<div className="card5">
+<div className="connect1">
+  Let's connect
+</div>
+<p className="starter">Enterprise Plan</p>
+<p className="para-11">Effortlessly customize and fine-tune services as your needs shift,ensuring the perfect tools for success</p>
+<div className="blue-bg1">
+<p className="yellow-p">Contact Us</p><img src="rightarrow.png" className="y-a"></img>
+
+</div>
+<p className="get1">What you'll get:</p>
+<img src="userhead.png" className="userhead6"></img>  
+    <p className="cardcontent-21">More than 75 Users</p>
+
+    <img src="doubletick.png" className="userhead7"></img>  
+    <p className="cardcontent-211">Cutomization of all<br></br>other features</p>
+    
+
+
+
+
+</div>
+<p className="para-blue"><sup >*</sup>some unique features are provided as add-ons with individual plans for each features</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+        
+        <div className="container-s">
+          <img src="bell.png" className="bell"></img>
+
+        </div>
+
+        <div className="container-s2">
+          
+        </div>
+
+        <div className="container-s3"><div className="plus">+</div>
+
+        </div>
+
+
+
+
+
+      </div>
+    </div>
+  );
+}
+
+export default Home;
